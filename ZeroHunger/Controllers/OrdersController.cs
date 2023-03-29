08b1d2a1fdd2f1cf411b96bf6125e7dfa5cc4d07
Sayem Mustak Hasan <mustakhasansayem@gmail.com>
2023-03-30 00:35:06 +0600

    Add project files.

---
+++ ZeroHunger/Controllers/OrdersController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ZeroHunger.Database;
+
+namespace ZeroHunger.Controllers
+{
+    public class OrdersController : Controller
+    {
+        [HttpGet]
+        public ActionResult History()
+        {
+            var db = new ZeroHungerEntities();
+            return View(db.Histories.ToList());
+        }
+
+    }
+}
